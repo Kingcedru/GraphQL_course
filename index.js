4d777ce22fa66947df80057f0e9bb6593e95ea9a
Kingcedru <byishimocedrick@gmail.com>
2024-04-14 11:42:36 +0200

    grouping related data

--- index.js
+++ index.js
@@ -12,21 +12,39 @@ const resolvers = {
     games() {
       return db.games;
     },
-    game(_, args){
-      return db.games.find((game)=> game.id === args.id)
+    game(_, args) {
+      return db.games.find((game) => game.id === args.id);
     },
     authors() {
       return db.authors;
     },
-    author(_, args){
-      return db.authors.find(author=> author.id === args.id)
+    author(_, args) {
+      return db.authors.find((author) => author.id === args.id);
     },
     reviews() {
       return db.reviews;
     },
-    review(_, args){
-      return db.reviews.find((review)=> review.id === args.id)
-    }
+    review(_, args) {
+      return db.reviews.find((review) => review.id === args.id);
+    },
+  },
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((r) => r.author_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((a) => a.id === parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((g) => g.id === parent.game_id);
+    },
   },
 };
 
--- schema.js
+++ schema.js
@@ -3,16 +3,20 @@ type Game {
     id: ID!
     title: String!
     platform: [String!]!
+    reviews: [Review!]
 }
 type Review{
     id: ID!
     rating: Int!
     content: String!
+    game: Game!
+    author: Author!
 }
 type Author {
     id: ID!
     name: String!
     verified: Boolean!
+    reviews: [Review!]
 }
 type Query {
     reviews: [Review]
